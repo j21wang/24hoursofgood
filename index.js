f0a55780191f23634a5c4d036c2e8aa4c454ff35
Joyce Wang <joycewang2002@yahoo.com>
2014-02-08 03:08:22 -0500

    new phone size

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-mainScreen();
-function mainScreen(){
-    $("#screen").text("SUP");
+function textScreen(){
+    $("#screen").html("<h1 id='contact'>Mom</h1><div id='textsBox'></div><input type='text' name='textMessage' class='textMessage' autocomplete='off'><button class='send'>Send</button><script src='text.js'></script>");
+}
+
+function gmailScreen(){
 }
