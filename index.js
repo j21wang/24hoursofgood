4ef630b5ae823227eb6c2541127c519cf93b7403
Joyce Wang <joycewang2002@yahoo.com>
2014-02-08 03:36:09 -0500

    email done

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ function textScreen(){
     $("#screen").html("<h1 id='contact'>Mom</h1><div id='textsBox'></div><input type='text' name='textMessage' class='textMessage' autocomplete='off'><button class='send'>Send</button><script src='text.js'></script>");
 }
 
-function gmailScreen(){
+function gmailScreen1(){
+    $("#screen").html("My Emails<h3 onclick='showInbox()'>Inbox</h3><h3 onclick='showImportant()'>Important</h3><h3 onclick='showSent()'>Sent Mail</h3><h3 onclick='showDrafts()'>Drafts</h3><h3>More</h3>");
 }
+
+gmailScreen1();
